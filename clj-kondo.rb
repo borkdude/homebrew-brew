24e625dec5dbd87e9222a8ec843998c7d24f4e2e
Michiel Borkent <michielborkent@gmail.com>
2019-05-12 21:32:35 +0200

    v2019.05.12-alpha

--- clj-kondo.rb
+++ clj-kondo.rb
@@ -2,14 +2,14 @@ class CljKondo < Formula
 
   desc "A minimal and opinionated linter for Clojure code that sparks joy."
   homepage "https://github.com/borkdude/clj-kondo"
-  version "2019.05.06-alpha"
+  version "2019.05.12-alpha"
 
   if OS.linux?
-    url "https://github.com/borkdude/clj-kondo/releases/download/v2019.05.06-alpha/clj-kondo-2019.05.06-alpha-linux-amd64.tar.gz"
-    sha256 "b2601d4a6c9f139a82c1894f250bc6f36158d74b55337162b9972445b46d142e"
+    url "https://github.com/borkdude/clj-kondo/releases/download/v2019.05.12-alpha/clj-kondo-2019.05.12-alpha-linux-amd64.tar.gz"
+    sha256 "b63514fd992e9a05bde3a673799a02018538dc39fd36063f2f225e45d4264275"
   else
-    url "https://github.com/borkdude/clj-kondo/releases/download/v2019.05.06-alpha/clj-kondo-2019.05.06-alpha-macos-amd64.tar.gz"
-    sha256 "94034bbc5b6231bacf60d70f9b2ece858e3a1331ac0521a3c13e5b6bb913e18e"
+    url "https://github.com/borkdude/clj-kondo/releases/download/v2019.05.12-alpha/clj-kondo-2019.05.12-alpha-macos-amd64.tar.gz"
+    sha256 "f78611cdedab0986c3392bb81a551bfd61bfe5f3c2333f9c9ea3853d9e379d16"
   end
 
   bottle :unneeded

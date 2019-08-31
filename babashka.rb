fdb7fdc8761854ab7a7ab11c422b4f130413d9a7
Michiel Borkent <michielborkent@gmail.com>
2019-08-31 21:20:13 +0200

    bb v0.0.14

--- babashka.rb
+++ babashka.rb
@@ -2,14 +2,14 @@ class Babashka < Formula
 
   desc "A pure, fast and limited version of Clojure in Clojure for shell scripting."
   homepage "https://github.com/borkdude/babashka"
-  version "0.0.13"
+  version "0.0.14"
 
   if OS.linux?
-    url "https://github.com/borkdude/babashka/releases/download/v0.0.13/babashka-0.0.13-linux-amd64.zip"
-    sha256 "282cfd188f5da18a0e733aa835052da38399a567b615b5a1733d85466584a5f7"
+    url "https://github.com/borkdude/babashka/releases/download/v0.0.14/babashka-0.0.14-linux-amd64.zip"
+    sha256 "7494581e5ac06cad3da69e58790e6789913cfbf82a34fc19a1ae41bd3022cf26"
   else
-    url "https://github.com/borkdude/babashka/releases/download/v0.0.13/babashka-0.0.13-macos-amd64.zip"
-    sha256 "d95da1eef11cf25332ac15038e9864984a907f47fb5215b7afedaa3daea1eadf"
+    url "https://github.com/borkdude/babashka/releases/download/v0.0.14/babashka-0.0.14-macos-amd64.zip"
+    sha256 "b7187ee7971519355c13dfb3627ad9b92e2a82d6b4e646cfe4d9a549686bea10"
   end
 
   bottle :unneeded

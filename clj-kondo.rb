932fb91f19a4a0988d8bdd692b236650c1b0dc08
Michiel Borkent <michielborkent@gmail.com>
2019-10-26 11:53:13 +0200

    v2019.10.26

--- clj-kondo.rb
+++ clj-kondo.rb
@@ -2,14 +2,14 @@ class CljKondo < Formula
 
   desc "A minimal and opinionated linter for Clojure code that sparks joy."
   homepage "https://github.com/borkdude/clj-kondo"
-  version "2019.10.11-alpha"
+  version "2019.10.26"
 
   if OS.linux?
-    url "https://github.com/borkdude/clj-kondo/releases/download/v2019.10.11-alpha/clj-kondo-2019.10.11-alpha-linux-amd64.zip"
-    sha256 "36b79ed7ec1889e239c62739e292b248f52525530c2a89d8396a07eb66e3f9f9"
+    url "https://github.com/borkdude/clj-kondo/releases/download/v2019.10.26/clj-kondo-2019.10.26-linux-amd64.zip"
+    sha256 "ec8176ca9860139eff7596182cb483d127311f2cf7e6f963417555752c6f5171"
   else
-    url "https://github.com/borkdude/clj-kondo/releases/download/v2019.10.11-alpha/clj-kondo-2019.10.11-alpha-macos-amd64.zip"
-    sha256 "f6d86b1dfd4d29a9d3e2482c840fbd30e11da5182c4f595a5e4b95ea0d813567"
+    url "https://github.com/borkdude/clj-kondo/releases/download/v2019.10.26/clj-kondo-2019.10.26-macos-amd64.zip"
+    sha256 "5aeadf3aec96cc79bc405ee181aa0ad54f85cd39b4278675bc684da50ba52a2f"
   end
 
   bottle :unneeded

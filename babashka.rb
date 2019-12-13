26c921d21bd0b783d6b3f53a5641a6abeb9c2df8
Michiel Borkent <michielborkent@gmail.com>
2019-12-13 18:38:25 +0100

    babashka v0.0.41

--- babashka.rb
+++ babashka.rb
@@ -1,21 +1,25 @@
 class Babashka < Formula
 
-  desc "A pure, fast and limited version of Clojure in Clojure for shell scripting."
+  desc "A Clojure babushka for the grey areas of Bash."
   homepage "https://github.com/borkdude/babashka"
-  version "0.0.39"
+  version "0.0.41"
 
   if OS.linux?
-    url "https://github.com/borkdude/babashka/releases/download/v0.0.39/babashka-0.0.39-linux-amd64.zip"
-    sha256 "2edc2f24785d5f36c1ea4021a776db0a4d1d39bad13ca60e68336a4963426e4f"
+    url "https://github.com/borkdude/babashka/releases/download/v0.0.41/babashka-0.0.41-linux-amd64.zip"
+    sha256 "d0900a5fe95e50cefce5a1892a77b42e478af87e284e5f68918455fb10c6d440"
   else
-    url "https://github.com/borkdude/babashka/releases/download/v0.0.39/babashka-0.0.39-macos-amd64.zip"
-    sha256 "6d05e6c8798b2c0c6ea5f1732293b0424decd0528677baaac5cee20db55c0abf"
+    url "https://github.com/borkdude/babashka/releases/download/v0.0.41/babashka-0.0.41-macos-amd64.zip"
+    sha256 "a0ebad0d7c9824f05ca072a6b8bc877727b8b060d1147e2ad703a612e8bf0755"
   end
 
   bottle :unneeded
 
   def install
     bin.install "bb"
+
+# maybe in a future release:
+#    bin.install "bbk"
+
   end
 
 end

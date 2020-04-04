b594686330a83c032a1c22c80a51b9291dbc5ca7
Michiel Borkent <michielborkent@gmail.com>
2020-04-04 16:38:00 +0200

    bb 79

--- babashka.rb
+++ babashka.rb
@@ -2,14 +2,14 @@ class Babashka < Formula
 
   desc "A Clojure babushka for the grey areas of Bash."
   homepage "https://github.com/borkdude/babashka"
-  version "0.0.78"
+  version "0.0.79"
 
   if OS.linux?
-    url "https://github.com/borkdude/babashka/releases/download/v0.0.78/babashka-0.0.78-linux-amd64.zip"
-    sha256 "556ec06783d22754a3aeed3427b8c6673d1bec58456e536d176fdfc0750d3848"
+    url "https://github.com/borkdude/babashka/releases/download/v0.0.79/babashka-0.0.79-linux-amd64.zip"
+    sha256 "11ecfef0d87a0cac0633b63502c35d13d9e4fe8cb9ef51dd29bafe7a5a4398f4"
   else
-    url "https://github.com/borkdude/babashka/releases/download/v0.0.78/babashka-0.0.78-macos-amd64.zip"
-    sha256 "6809dabcf420f815fd7c29f624c8b2520a697064d3052f52af21067aec86ee27"
+    url "https://github.com/borkdude/babashka/releases/download/v0.0.79/babashka-0.0.79-macos-amd64.zip"
+    sha256 "c1c16c03c2eaa95c2f0b7cf936c789bc0d5851cd6f9784e4e40ce147c01afdc9"
   end
 
   bottle :unneeded

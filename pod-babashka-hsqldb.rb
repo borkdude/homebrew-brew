682f43ec649e61696f8eb36c570a9e5edb852099
Michiel Borkent <michielborkent@gmail.com>
2020-05-09 21:48:06 +0200

    hsqldb

--- pod-babashka-hsqldb.rb
+++ pod-babashka-hsqldb.rb
@@ -1,15 +1,15 @@
 class PodBabashkaHsqldb < Formula
 
   desc "Babashka pod for HSQLDB."
-  homepage "https://github.com/borkdude/pod-babashka-hsqldb"
+  homepage "https://github.com/babashka/pod-babashka-hsqldb"
   version "0.0.1-SNAPSHOT"
 
   if OS.linux?
     url "https://github.com/borkdude/pod-babashka-hsqldb/releases/download/v0.0.1-SNAPSHOT/pod-babashka-hsqldb-0.0.1-SNAPSHOT-linux-amd64.zip"
-    sha256 "d53938633967c1a9494f19bf445f44f5bf75d0781d81e11a0c25c3b01821dd59"
+    sha256 "e9b97c6750c00c6a13bc146ca1781b38e9ec1efbe2aebd61a0c3da5df64846d7"
   else
     url "https://github.com/borkdude/pod-babashka-hsqldb/releases/download/v0.0.1-SNAPSHOT/pod-babashka-hsqldb-0.0.1-SNAPSHOT-macos-amd64.zip"
-    sha256 "702065727a004789ea96d5926783ea2bfaa107ce7ae2dca2d4988454abf82e09"
+    sha256 "48b0b29b2f520bc28ec85bf664c84912f9cea29c4e4e484fc7b811ce600a94af"
   end
 
   bottle :unneeded

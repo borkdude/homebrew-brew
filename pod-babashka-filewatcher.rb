b4a3900fbdc4cd36ea1399194d88c0d42b3dd79d
Michiel Borkent <michielborkent@gmail.com>
2020-05-10 20:31:03 +0200

    wip

--- pod-babashka-filewatcher.rb
+++ pod-babashka-filewatcher.rb
@@ -5,10 +5,10 @@ class PodBabashkaFilewatcher < Formula
   version "0.0.1-SNAPSHOT"
 
   if OS.linux?
-    url "https://github.com/borkdude/pod-babashka-filewatcher/releases/download/v0.0.1-SNAPSHOT/pod-babashka-filewatcher-0.0.1-SNAPSHOT-linux-amd64.zip"
+    url "https://github.com/babashka/pod-babashka-filewatcher/releases/download/v0.0.1-SNAPSHOT/pod-babashka-filewatcher-0.0.1-SNAPSHOT-linux-amd64.zip"
     sha256 "087c7ee2b864803b3cbb09396b39d76df7f0ecf25ab2b29c564ca4026b866209"
   else
-    url "https://github.com/borkdude/pod-babashka-filewatcher/releases/download/v0.0.1-SNAPSHOT/pod-babashka-filewatcher-0.0.1-SNAPSHOT-macos-amd64.zip"
+    url "https://github.com/babashka/pod-babashka-filewatcher/releases/download/v0.0.1-SNAPSHOT/pod-babashka-filewatcher-0.0.1-SNAPSHOT-macos-amd64.zip"
     sha256 "264dc8a962c044623dfb926015991ddbd68a67ca9136f0be8d986b6f58b2ed9d"
   end
 

9e93c8a9f27a67bc6dd7b2cdbdb74eacf9427973
Michiel Borkent <michielborkent@gmail.com>
2020-05-20 23:32:55 +0200

    filewatcher

--- pod-babashka-filewatcher.rb
+++ pod-babashka-filewatcher.rb
@@ -6,10 +6,10 @@ class PodBabashkaFilewatcher < Formula
 
   if OS.linux?
     url "https://github.com/babashka/pod-babashka-filewatcher/releases/download/v0.0.1-SNAPSHOT/pod-babashka-filewatcher-0.0.1-SNAPSHOT-linux-amd64.zip"
-    sha256 "e4a7a509a40bdb3c13f8ab9bbc59c693f08b53b5844c72d2cadc2498fd70d8cc"
+    sha256 "2f18a63e04cb83c16674532996760b300107bfec2cc57fc1326c853be721b0c2"
   else
     url "https://github.com/babashka/pod-babashka-filewatcher/releases/download/v0.0.1-SNAPSHOT/pod-babashka-filewatcher-0.0.1-SNAPSHOT-macos-amd64.zip"
-    sha256 "6cff970eaed1fadaddd5d9f7ade3d82df78e4b656c74629016bbf3bcc5a2163c"
+    sha256 "9c3729fb87d1dbc665740815e352f907d38163071f917c5801f6ba759cf733bc"
   end
 
   bottle :unneeded

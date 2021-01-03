a6f86f0162bd67f4b1dde75fe451004d4af5ae3f
Michiel Borkent <michielborkent@gmail.com>
2021-01-03 11:08:47 +0100

    puget v0.0.2

--- puget.rb
+++ puget.rb
@@ -2,14 +2,14 @@ class Puget < Formula
 
   desc "Pretty-print EDN with colors."
   homepage "https://github.com/borkdude/puget-cli"
-  version "0.0.1"
+  version "0.0.2"
 
   if OS.linux?
-    url "https://github.com/borkdude/puget-cli/releases/download/v0.0.1/puget-cli-0.0.1-linux-amd64.zip"
-    sha256 "4287c9f9d013b0a7cac797205318e51e93455d37cb24052fd64bfd5816417ae1"
+    url "https://github.com/borkdude/puget-cli/releases/download/v0.0.2/puget-cli-0.0.2-linux-amd64.zip"
+    sha256 "a7a92f4bccf712f8ddba6914cdd3aa14b621558bdd105a16b101b5f2f0700d04"
   else
-    url "https://github.com/borkdude/puget-cli/releases/download/v0.0.1/puget-cli-0.0.1-macos-amd64.zip"
-    sha256 "f11460a98120ee41d479c93989f7954d3a6b3a1095db4b314e7270f54171a8aa"
+    url "https://github.com/borkdude/puget-cli/releases/download/v0.0.2/puget-cli-0.0.2-macos-amd64.zip"
+    sha256 "84ec70977701424128a9767c3b1f4f50ef40458a6c4ddf28e8315e692982411b"
   end
 
   bottle :unneeded

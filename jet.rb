01ec960da5da3864d6cb854e13fa0c5d83401618
Michiel Borkent <michielborkent@gmail.com>
2021-04-04 21:36:00 +0200

    jet

--- jet.rb
+++ jet.rb
@@ -2,14 +2,14 @@ class Jet < Formula
 
   desc "Transform JSON into EDN into Transit."
   homepage "https://github.com/borkdude/jet"
-  version "0.0.14"
+  version "0.0.15"
 
   if OS.linux?
-    url "https://github.com/borkdude/jet/releases/download/v0.0.14/jet-0.0.14-linux-amd64.zip"
-    sha256 "5b9e6dcafbf36895c310253720f0644ac4d5395375eb629e063dc7378dee4949"
+    url "https://github.com/borkdude/jet/releases/download/v0.0.15/jet-0.0.15-linux-amd64.zip"
+    sha256 "a7a87e11fb98b9b36501405aeda9d285ce974539282d51e0a3101c5bbb82cac0"
   else
-    url "https://github.com/borkdude/jet/releases/download/v0.0.14/jet-0.0.14-macos-amd64.zip"
-    sha256 "443206c0ac92af4c3720cd335886adbfac1f458f01f1000ab619041cf3cd61d1"
+    url "https://github.com/borkdude/jet/releases/download/v0.0.15/jet-0.0.15-macos-amd64.zip"
+    sha256 "40da55fa93175c795f65eed36094cd670131d6ceb18a0e1886c1b0cab02e9846"
   end
 
   bottle :unneeded

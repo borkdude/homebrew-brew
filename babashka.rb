b2cc934a765618dd07b32a3492c11665e6eb32da
Michiel Borkent <michielborkent@gmail.com>
2022-02-24 14:37:46 +0100

    Fix sha

--- babashka.rb
+++ babashka.rb
@@ -6,7 +6,7 @@ class Babashka < Formula
 
   if OS.linux?
     url "https://github.com/borkdude/babashka/releases/download/v0.7.6/babashka-0.7.6-linux-amd64.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "bfdb05eb74cbba8e722a4c31eb4aea7b22255be20ea560de2519d28f3c18d704"
   else
     url "https://github.com/borkdude/babashka/releases/download/v0.7.6/babashka-0.7.6-macos-amd64.tar.gz"
     sha256 "65182c55b9a54298e913c87033ae141c80f7a0d9f15f741614ec1c855b77f063"

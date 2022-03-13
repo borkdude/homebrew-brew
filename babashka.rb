e7617307757bd0fe1da0eeb122aa220d4264efda
Michiel Borkent <michielborkent@gmail.com>
2022-03-13 20:24:09 +0100

    bb

--- babashka.rb
+++ babashka.rb
@@ -1,15 +1,15 @@
 class Babashka < Formula
   desc "Native, fast starting Clojure interpreter for scripting."
   homepage "https://github.com/babashka/babashka"
-  version "0.7.7"
+  version "0.7.8"
   license "EPL-1.0"
 
   if OS.linux?
-    url "https://github.com/borkdude/babashka/releases/download/v0.7.7/babashka-0.7.7-linux-amd64.tar.gz"
-    sha256 "218c0c73cfea6a9a66ca1b3a98fa8005fc4c1661d6224c476305f5766f7ba4da"
+    url "https://github.com/borkdude/babashka/releases/download/v0.7.8/babashka-0.7.8-linux-amd64.tar.gz"
+    sha256 "4cdcf70317157a99059d328c6ab04a051e0cc132d37ed6ec5de81e34b044ac6b"
   else
-    url "https://github.com/borkdude/babashka/releases/download/v0.7.7/babashka-0.7.7-macos-amd64.tar.gz"
-    sha256 "d7d5384403ff7622565d91c0a2913e8be3de1f4e94449c2db89f184830a16c75"
+    url "https://github.com/borkdude/babashka/releases/download/v0.7.8/babashka-0.7.8-macos-amd64.tar.gz"
+    sha256 "a240cc014f28d7bc5e37f750c3d0bebf423f198b140c62694676bad844f1c59f"
   end
 
   def install

3120186d0d74c6c0cf4d5c062f5d2dc9d81067e5
Michiel Borkent <michielborkent@gmail.com>
2022-06-08 20:00:03 +0200

    babashka

--- babashka.rb
+++ babashka.rb
@@ -1,15 +1,19 @@
 class Babashka < Formula
   desc "Native, fast starting Clojure interpreter for scripting."
   homepage "https://github.com/babashka/babashka"
-  version "0.8.2"
+  version "0.8.156"
   license "EPL-1.0"
 
   if OS.linux?
-    url "https://github.com/borkdude/babashka/releases/download/v0.8.2/babashka-0.8.2-linux-amd64.tar.gz"
-    sha256 "8ebc5055557878f199d2d5346ae91d28341ee5da9c647bb3e95b394b1344df67"
+    url "https://github.com/borkdude/babashka/releases/download/v0.8.156/babashka-0.8.156-linux-amd64.tar.gz"
+    sha256 "f89784c03eaf271f1fb40d078e318df11ddbf839da4c20decaa3e9d26fa7b53e"
   else
-    url "https://github.com/borkdude/babashka/releases/download/v0.8.2/babashka-0.8.2-macos-amd64.tar.gz"
-    sha256 "f10adb26ab68a364f8d5811b2c01c197baa10d8682e726d185a320be8b534497"
+    if Hardware::CPU.arm?
+      url "https://github.com/borkdude/babashka/releases/download/v0.8.156/babashka-0.8.156-macos-aarch64.tar.gz"
+      sha256 "94fa3e4fb0dee9e8f129cf1b444a1a96329310e501d6d2a257e0608495905458"
+    else url "https://github.com/borkdude/babashka/releases/download/v0.8.156/babashka-0.8.156-macos-amd64.tar.gz"
+      sha256 "e4722dff56405363e8cc9d203ac000e77c22e74665ec3c45f0233d0fb534811e"
+    end
   end
 
   def install
@@ -21,6 +25,6 @@ class Babashka < Formula
 
   test do
     assert_equal "hello\n",
-      pipe_output("#{bin}/bb -e '(println \"hello\")'")
+                 pipe_output("#{bin}/bb -e '(println \"hello\")'")
   end
 end

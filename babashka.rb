f7c801102ae93013aba1d48659674961215be352
Michiel Borkent <michielborkent@gmail.com>
2022-09-24 14:34:19 +0200

    fix brew

--- babashka.rb
+++ babashka.rb
@@ -10,7 +10,7 @@ class Babashka < Formula
   else
     if Hardware::CPU.arm?
       url "https://github.com/borkdude/babashka/releases/download/v0.10.163/babashka-0.10.163-macos-aarch64.tar.gz"
-      sha256 "3ba11a506a98400913d72be2294adf2ff66a87a8dc1b791569e4368e39921165"
+      sha256 "21379368c0b9674a1656e9786191e6e496ab57b08242e621fa76b76e93056d1f"
     else url "https://github.com/borkdude/babashka/releases/download/v0.10.163/babashka-0.10.163-macos-amd64.tar.gz"
       sha256 "5dea13586c7d54db4fb133f89bcc5c1c18b64620841abf8ae9f31bbf7db57c5b"
     end

e04c8110b7d1f616c3cabb3d57a63260378ca8c3
Michiel Borkent <michielborkent@gmail.com>
2023-04-26 21:25:08 +0200

    jet

--- jet.rb
+++ jet.rb
@@ -2,17 +2,17 @@ class Jet < Formula
 
   desc "Transform JSON into EDN into Transit."
   homepage "https://github.com/borkdude/jet"
-  version "0.4.23"
+  version "0.5.25"
 
   if OS.linux?
-    url "https://github.com/borkdude/jet/releases/download/v0.4.23/jet-0.4.23-linux-amd64.tar.gz"
-    sha256 "4399f48aaac89a04c36e0261009d163621399a708b0a70d7f94183b3fdb872a2"
+    url "https://github.com/borkdude/jet/releases/download/v0.5.25/jet-0.5.25-linux-amd64.tar.gz"
+    sha256 "924ad772188bd7412f4e5bdd99eb8a48db5055056c695d47fe43c7a9c2cbd03b"
   else
     if Hardware::CPU.arm?
-      url "https://github.com/borkdude/jet/releases/download/v0.4.23/jet-0.4.23-macos-aarch64.tar.gz"
-      sha256 "974a7a649a924b6fc21c0e32686826a46ac1be8392b782bda3a43fa1953834fe"
-    else url "https://github.com/borkdude/jet/releases/download/v0.4.23/jet-0.4.23-macos-amd64.tar.gz"
-      sha256 "47bff82725ca6b6e82a88e93af5c75d5b9a213392f10b25334ee3a87972072ca"
+      url "https://github.com/borkdude/jet/releases/download/v0.5.25/jet-0.5.25-macos-aarch64.tar.gz"
+      sha256 "687872f05e25e35792d09e2d8375d4d545238fae75ef8549e64382a3b690f88f"
+    else url "https://github.com/borkdude/jet/releases/download/v0.5.25/jet-0.5.25-macos-amd64.tar.gz"
+      sha256 "59f82d42032a322286b3c9bad2b3a210d57539c2146e09a0288793bbc9673f94"
     end
   end
 

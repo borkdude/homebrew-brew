d1cd29b0035797afd9fa115aa5fbcb83edb23ffd
Michiel Borkent <michielborkent@gmail.com>
2025-10-02 15:36:58 +0200

    bb

--- babashka.rb
+++ babashka.rb
@@ -1,23 +1,23 @@
 class Babashka < Formula
   desc "Native, fast starting Clojure interpreter for scripting."
   homepage "https://github.com/babashka/babashka"
-  version "1.12.208"
+  version "1.12.209"
   license "EPL-1.0"
 
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/babashka/babashka/releases/download/v1.12.208/babashka-1.12.208-linux-aarch64-static.tar.gz"
-      sha256 "ff623c0909b8ec7e7e3f6ed88846f1cfc9fe7404d6fdd88addaa4b611cfe9d99"
+      url "https://github.com/babashka/babashka/releases/download/v1.12.209/babashka-1.12.209-linux-aarch64-static.tar.gz"
+      sha256 "177c18b6ca00c1708070007a53777ef4c9ffe0bbaa2c66c1426312db645eeba9"
     else
-      url "https://github.com/babashka/babashka/releases/download/v1.12.208/babashka-1.12.208-linux-amd64-static.tar.gz"
-      sha256 "424c0be8be3932ecf16151e115c126e386931cd2ac8706afd99599f03cdb7129"
+      url "https://github.com/babashka/babashka/releases/download/v1.12.209/babashka-1.12.209-linux-amd64-static.tar.gz"
+      sha256 "6f34a2c65266b10413ea4a49c1243a0126d4dcd7f64d14bc3ce781f134fe4c9a"
     end
   else
     if Hardware::CPU.arm?
-      url "https://github.com/babashka/babashka/releases/download/v1.12.208/babashka-1.12.208-macos-aarch64.tar.gz"
-      sha256 "118d3b857567fc515bf2c505689c234a881841f80a3d662bb79b60412ff378e4"
-    else url "https://github.com/babashka/babashka/releases/download/v1.12.208/babashka-1.12.208-macos-amd64.tar.gz"
-      sha256 "6763a6f93049abcd1c19980fb93c13b594c6894a8865e6d26c12a358329630b3"
+      url "https://github.com/babashka/babashka/releases/download/v1.12.209/babashka-1.12.209-macos-aarch64.tar.gz"
+      sha256 "92ec4624af3ce1fe09c177835836f23e60d018678c30ffcb83c1985c3a9c6d4f"
+    else url "https://github.com/babashka/babashka/releases/download/v1.12.209/babashka-1.12.209-macos-amd64.tar.gz"
+      sha256 "b02f906697b1ede016e96a3b340ea40e52380eef52307abc8a114210bfe89c44"
     end
   end
 

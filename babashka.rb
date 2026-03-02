a1cb338782123e28dc6a55a1aa7e0c5d46582514
Michiel Borkent <michielborkent@gmail.com>
2026-03-02 21:06:58 +0100

    bb

--- babashka.rb
+++ babashka.rb
@@ -1,23 +1,23 @@
 class Babashka < Formula
   desc "Native, fast starting Clojure interpreter for scripting."
   homepage "https://github.com/babashka/babashka"
-  version "1.12.215"
+  version "1.12.216"
   license "EPL-1.0"
 
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/babashka/babashka/releases/download/v1.12.215/babashka-1.12.215-linux-aarch64-static.tar.gz"
-      sha256 "e444283cdcc4b809fd409da56cf3791a60ed57eeef1f6a4c3ed186daf81de40e"
+      url "https://github.com/babashka/babashka/releases/download/v1.12.216/babashka-1.12.216-linux-aarch64-static.tar.gz"
+      sha256 "3c76a27121599430328abfdc81b7c8007402db8199be560e8418a6320298412a"
     else
-      url "https://github.com/babashka/babashka/releases/download/v1.12.215/babashka-1.12.215-linux-amd64-static.tar.gz"
-      sha256 "b1b975a54bff97d2ec1b0d712b34368d6add00bf5d3ca56fb2fa5f8f88526d45"
+      url "https://github.com/babashka/babashka/releases/download/v1.12.216/babashka-1.12.216-linux-amd64-static.tar.gz"
+      sha256 "3e85859b30f18a2dcccdc80453a8a3f811254f953c70dfbc0b731a2fb8476ba3"
     end
   else
     if Hardware::CPU.arm?
-      url "https://github.com/babashka/babashka/releases/download/v1.12.215/babashka-1.12.215-macos-aarch64.tar.gz"
-      sha256 "a7e791110f3f873e3e26264eb471701d0bb5d8e1d1377b7d3132559fc4296417"
-    else url "https://github.com/babashka/babashka/releases/download/v1.12.215/babashka-1.12.215-macos-amd64.tar.gz"
-      sha256 "1ece4f02092b838372519b7a704d78a352089d028a59a30c94be87094f36a09a"
+      url "https://github.com/babashka/babashka/releases/download/v1.12.216/babashka-1.12.216-macos-aarch64.tar.gz"
+      sha256 "91499b3f430038f9b40e433215256a6e5392942780dca9984d493d2bcca7055d"
+    else url "https://github.com/babashka/babashka/releases/download/v1.12.216/babashka-1.12.216-macos-amd64.tar.gz"
+      sha256 "8844da19b779b7ad5685cbea7622a1453176170cea104a64c650cb0d0898b64d"
     end
   end
 

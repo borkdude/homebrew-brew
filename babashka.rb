f876792d3d6c3cdf59541f17050e914d59b21803
Michiel Borkent <michielborkent@gmail.com>
2026-04-20 15:42:45 +0200

    bb

--- babashka.rb
+++ babashka.rb
@@ -1,23 +1,23 @@
 class Babashka < Formula
   desc "Native, fast starting Clojure interpreter for scripting."
   homepage "https://github.com/babashka/babashka"
-  version "1.12.217"
+  version "1.12.218"
   license "EPL-1.0"
 
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/babashka/babashka/releases/download/v1.12.217/babashka-1.12.217-linux-aarch64-static.tar.gz"
-      sha256 "f0c01a29232a79ddc550b6cbd1387e255b3e20fa888c7f40499708e3c3c3200d"
+      url "https://github.com/babashka/babashka/releases/download/v1.12.218/babashka-1.12.218-linux-aarch64-static.tar.gz"
+      sha256 "e9e9190afb0dd33abbcd3aa6c1382184a88a5498800324719be3be6e1aa68302"
     else
-      url "https://github.com/babashka/babashka/releases/download/v1.12.217/babashka-1.12.217-linux-amd64-static.tar.gz"
-      sha256 "137bffe4555020af81abe56dffeadd6c3cd3fa3e998706ae1f5d2edb2f2ec486"
+      url "https://github.com/babashka/babashka/releases/download/v1.12.218/babashka-1.12.218-linux-amd64-static.tar.gz"
+      sha256 "7bd028cc794732ffde3da31ce4379840893c8e54f1046f92a8dfc4f4b3cddaf8"
     end
   else
     if Hardware::CPU.arm?
-      url "https://github.com/babashka/babashka/releases/download/v1.12.217/babashka-1.12.217-macos-aarch64.tar.gz"
-      sha256 "c87637b58fe214a904374593941227a938c91a98962fe12bcd9ec8b666f7b8ca"
-    else url "https://github.com/babashka/babashka/releases/download/v1.12.217/babashka-1.12.217-macos-amd64.tar.gz"
-      sha256 "a5a1c685fbc6bccbb9bbfe1e3ab698c47d0d92f2ec3a02a9bbbb49e527cbfb1d"
+      url "https://github.com/babashka/babashka/releases/download/v1.12.218/babashka-1.12.218-macos-aarch64.tar.gz"
+      sha256 "5bc992f39692b707403fc322e860fc82017da7de4a84a32267abb4d50a0c5f9d"
+    else url "https://github.com/babashka/babashka/releases/download/v1.12.218/babashka-1.12.218-macos-amd64.tar.gz"
+      sha256 "2b7640a919b79406142b12c488ee83f7ba070c04b82bee8f74ad4eab074ddaeb"
     end
   end
 
